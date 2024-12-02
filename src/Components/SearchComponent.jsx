dc9b46abf647b3a3f721773af89be240e16e738f
Ahmedrabie818 <rabieahmed027@gmail.com>
2024-12-02 23:03:42 +0200

    Update search component

--- src/Components/SearchComponent.jsx
+++ src/Components/SearchComponent.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useCallback } from "react";
 import { useRecoilState } from "recoil";
 import axios from "axios";
 import { searchResultsState, searchTextState } from "../state/atoms";
+
+function debounce(func, delay) {
+  let timeout;
+  return (...args) => {
+    timeout = clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
 
 const SearchComponent = () => {
   const [searchText, setSearchText] = useRecoilState(searchTextState);
   const [searchResults, setSearchResults] = useRecoilState(searchResultsState);
 
-  async function fetchSearchResults(query) {
+  async function fetchResults(query) {
     try {
       const response = await axios.get(
         `https://itunes.apple.com/search?term=${query}&limit=10`
@@ -18,16 +28,27 @@ const SearchComponent = () => {
     }
   }
 
-  useEffect(() => {
-    fetchSearchResults(searchText);
-  }, [searchText]);
+  const debouncedFetchResults = useCallback(
+    debounce((query) => {
+      if (query.trim() !== "") {
+        fetchResults(query);
+      }
+    }, 500),
+    []
+  );
+
+  function handelChange(e) {
+    let query = e.target.value;
+    setSearchText(query);
+    debouncedFetchResults(query);
+  }
 
   return (
     <input
       type="text"
       placeholder="Search iTunes..."
       value={searchText}
-      onChange={(e) => setSearchText(e.target.value)}
+      onChange={handelChange}
       className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
   );
